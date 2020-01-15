a58cebd95376416a9e7481274e943d135580a6ab
Joao Conde <joaodiasconde@gmail.com>
2020-01-15 18:49:02 +0100

    Count unique minimizers and their frequencies

--- minimize.cpp
+++ minimize.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -8,6 +9,7 @@ using namespace std;
 
 //todo positions
 //discard top frequency
+//default values for (k, w, f) should be (15, 5, 0.001)
 string findMinimizer(string str, int k){
     vector<string> tmp;
     for(int i = 0; i < str.size() - k + 1; i++) tmp.push_back(str.substr(i, k));
@@ -41,24 +43,36 @@ int main(int argc, char** argv){
 
     cout << "Extracting minimizers from '" << inputFileName << "' for k=" << k << " and w=" << w << endl;
 
-    vector<string> minimizers;
+    map<string, int> minimizerFrequencies;
 
     //left minimizers
-    for(string str: findLeftEnd(genome.substr(0, l), k, l)) 
-        minimizers.push_back(str);
+    for(string str: findLeftEnd(genome.substr(0, l), k, l)){
+        if(minimizerFrequencies.find(str) != minimizerFrequencies.end())
+            minimizerFrequencies[str]++;
+        else
+            minimizerFrequencies[str] = 0;
+    }
 
     //interior minimizers
-    for(int i = 0; i < genome.size() - l + 1; i++)
-        minimizers.push_back(findMinimizer(genome.substr(i, l), k));
+    for(int i = 0; i < genome.size() - l + 1; i++){
+        string str = findMinimizer(genome.substr(i, l), k);
+        if(minimizerFrequencies.find(str) != minimizerFrequencies.end())
+            minimizerFrequencies[str]++;
+        else
+            minimizerFrequencies[str] = 0;
+    }
 
     //right minimizers
-    int gSize = genome.size();
-    for(string str: findRightEnd(genome.substr(gSize - l + 1, l + 1), k, l)) 
-        minimizers.push_back(str);
+    for(string str: findRightEnd(genome.substr(genome.size() - l + 1, l + 1), k, l)) {
+        if(minimizerFrequencies.find(str) != minimizerFrequencies.end())
+            minimizerFrequencies[str]++;
+        else
+            minimizerFrequencies[str] = 0;
+    }
 
     cout << "Writing minimizers to '" << outputFileName << "'" << endl;
     ofstream output(outputFileName);
-    for(string minimizer: minimizers) output << minimizer << endl;
+    for(auto minFreq: minimizerFrequencies) output << minFreq.first << " " << minFreq.second << endl;
     output.close();
 
     cout << "Done!" << endl;
